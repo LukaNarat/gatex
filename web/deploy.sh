05231ebc9ae44ad5f72932ba11a91228f99c8afb
Luka Narat <luka.narat@gmail.com>
2017-09-26 13:40:32 +0200

    automatic push

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,18 +1,17 @@
 
 #!/bin/sh
 
-while getopts ":a:" opt; do
+SSH=pi@192.168.1.110
+
+while getopts ":r" opt; do
   case $opt in
     a)
-      echo "-a was triggered, Parameter: $OPTARG" >&2
+      echo "------ Remote work push! -------" >&2
+      SSH="luma.madhacker.biz -p 1337"
+      echo "$ALPHA"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
       ;;
   esac
 done
@@ -24,7 +23,7 @@ git commit -m "automatic push"
 git push origin master
 
 
-ssh -t pi@192.168.1.110 << HERE
+ssh -t $SSH << HERE
  cd /var/www/html/gatex
  sudo git pull
 HERE
