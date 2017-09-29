aa66f2cc57f1ab784976462db138788383b00ab2
Luka Narat <luka.narat@gmail.com>
2017-09-29 17:15:35 +0200

    automatic push

--- web/functions.js
+++ web/functions.js
@@ -1,10 +1,47 @@
-     // $(document).ready(function(){ 
-     //  onloadstatecheck();
+      $(document).ready(function(){ 
+       getCookie();
+       doSomething();
 
-     // }) 
+      }) 
 
 
 
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+function doSomething() {
+    var myCookie = getCookie("gatex");
+
+    if (myCookie == null) {
+        // do cookie doesn't exist stuff;
+          console.log("no cookie") 
+          window.location = "no.html";
+
+
+    }
+    else {
+         console.log("cookie found") 
+ // do cookie exists stuff
+    }
+}
 // function onloadstatecheck () {
 //       $.get("statecheck/callingactive.html")
         
