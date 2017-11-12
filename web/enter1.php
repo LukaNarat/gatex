038b22281aafaaf82bd1a3844ab2e61683450543
Luka Narat <luka.narat@gmail.com>
2017-11-12 23:23:04 +0100

    automatic push

--- web/enter1.php
+++ web/enter1.php
@@ -2,16 +2,7 @@
 
 function bb()
 {
-    $to      = 'gatexhack@gmail.com';
-    $subject = 'maryIn';
-    $message = 'Entering Gate 1';
-    $headers = 'From: webmaster@example.com' . "\r\n" .
-        'Reply-To: webmaster@example.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-
-    echo 'Email Sent.';
+shell_exec("./maryIn.sh");
 }
 
 
